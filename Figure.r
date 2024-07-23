02555a54747c70a82a160717895784e1bb7f8505
William Wang <56852428+bill-pipi@users.noreply.github.com>
2024-07-22 21:19:03 -0400

    update oud

--- Figure.r
+++ Figure.r
@@ -1,8 +1,8 @@
 library(forestplot)
 
 workdir <- "."
-#datafile <- file.path(workdir,"Figure2a.csv")
-datafile <- file.path(workdir,"Figure2b.csv")
+datafile <- file.path(workdir,"Figure2a.csv")
+#datafile <- file.path(workdir,"Supplement Figure 2.csv")
 
 data <- read.csv(datafile, stringsAsFactors=FALSE,row.names=NULL)
 
@@ -12,32 +12,32 @@ np <- ifelse(!is.na(data$case_group), paste(data$case_group," (",data$p_val,")",
 
 ## The rest of the columns in the table. 
 tabletext <- cbind(c("Size/group",data$casecount),
-                   c("Exposure\ngroup",data$controlcount),
-                   c("Comparison\ngroup",data$blah),
-                   c("cases (overall risk)\n(Exposure)",data$case_group),
-                   c("cases (overall risk)\n(Comparison)",data$control_group),
-                 #  c("RD (95% CI)",data$blah2),
+                   c("Exposure",data$controlcount),
+                   c("Comparison",data$blah),
+                   c("Cases (overall risk)\n(Exposure)\n",data$case_group),
+                   c("Cases (overall risk)\n(Comparison)\n",data$control_group),
+                   c("Risk difference\n(95% CI)",data$blah2),
                    c("        HR (95% CI)",data$CI)
 )
 
-#pdf(file.path(workdir,"Figure2a.pdf"),  onefile=FALSE, width=11.5, height=6)
-pdf(file.path(workdir,"Figure2b.pdf"),  onefile=FALSE, width=11.5, height=6)
-
+pdf(file.path(workdir,"Figure2a.pdf"),  onefile=FALSE, width=13, height=6.5)
+#pdf(file.path(workdir,"Supplement Figure 2.pdf"),  onefile=FALSE, width=13, height=6.5)
 
 forestplot(labeltext=tabletext, 
            graphwidth = unit(70, 'mm'),
-           graph.pos=6,
+           graph.pos=7,
           #is.summary=c(TRUE,TRUE,rep(FALSE, 6), TRUE,rep(FALSE,10)),
           is.summary=c(TRUE,rep(FALSE, 150)),
           
            mean=c(NA,data$RR), 
            lower=c(NA,data$down), upper=c(NA,data$up),
           
-          #title ="12 month risk of opioid overdose in patients with T2DM and OUD\n(Comparision between matched semaglutide vs other anti-diabetes medications groups)", 
-         title ="12 month risk of congenital malformations, deformations, and chromosomal abnormalities in patients with T2DM and OUD\n(Comparision between matched semaglutide vs other anti-diabetes medications groups)", 
-         
-           xticks=log(c(0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1,2,3,4,5)),
-          #xticks=log(c(0.3,0.4,0.5,0.6,0.7,0.8,0.9,1,2,3)),
+          title ="12 month risk of opioid overdose in patients with comorbid T2DM and OUD\n(Comparision between matched semaglutide vs other anti-diabetic medications groups)", 
+          #title ="12 month risk of medical encounters for congenital malformations, deformations, and chromosomal abnormalities\n in patients with comorbid T2DM and OUD\n(Comparision between matched semaglutide vs other anti-diabetes medications groups)", 
+          
+          
+           xticks=log(c(0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1,2,3,4,5,10)),
+         # xticks=log(c(0.3,0.4,0.5,0.6,0.7,0.8,0.9,1,2,3)),
           
            
            #xticks=10^(seq(-1,1,0.1)),
@@ -63,7 +63,7 @@ forestplot(labeltext=tabletext,
           col=fpColors(box="black", lines="black", zero = "black"),
           lwd.zero = 001,
           lwd.xaxis = 0.7,
-          mar = unit(c(0,0,0,0), "mm"),
+          mar = unit(c(00,0,0,0), "mm"),
           zero=1, cex=0.01, lineheight = unit(2, "mm"), boxsize=0.15, colgap=unit(3,"mm"),
           lwd.ci=0.8, ci.vertices=TRUE, ci.vertices.height = 0.15)
 
